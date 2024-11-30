729cd442d0f0a919f758476c3f5749bafda46b07
piinko <piinko@deeplay.io>
2024-11-30 14:46:27 +0700

    Добавлена задача Работа с объектами

--- JSArray/Array.js
+++ JSArray/Array.js
@@ -1,34 +1,40 @@
-function main() {
-    const array_1 = createArray(10);
-    console.log("Исходный массив", array_1);
+const appMameSpace = (() => {
+    const createArray = (length) => Array.from({length}, (_, i) => i + 1);
 
-    const sortedArray = sortArrayDescending(array_1);
-    console.log("Отсортированный массив в порядке убывания: ", sortedArray);
+    const sortArrayDescending = (array) => array.sort((a, b) => b - a);
 
-    const firstFiveSubArray = getFirstFiveSubArray(array_1);
-    console.log("Первые 5 элементов массива: ", firstFiveSubArray);
+    const getFirstFiveSubArray = (array) => array.slice(0, 5);
 
-    const lastFiveSubArray = getLastFiveSubArray(array_1);
-    console.log("Последние 5 элементов массива: ", lastFiveSubArray);
+    const getLastFiveSubArray = (array) => array.slice(-5);
 
-    const evenNumbersSum = getSumEvenNumbers(array_1);
-    console.log("Сумма чётных чисел массива", evenNumbersSum);
+    const getSumEvenNumbers = (array) => array.filter(num => num % 2 === 0).reduce((sum, num) => sum + num, 0);
 
-    const array_2 = createArray(100);
-    const squaresArray = getSquaresOfEvenNumbers(array_2);
-    console.log("Квадраты чётных чисел массива: ", squaresArray)
-}
+    const getSquaresOfEvenNumbers = (array) => array.filter(num => num % 2 === 0).map(num => num * num);
 
-const createArray = (length) => Array.from({length}, (_, i) => i + 1);
+    const main = () => {
+        const array_1 = createArray(10);
+        console.log("Исходный массив", array_1);
 
-const sortArrayDescending = (array) => array.sort((a, b) => b - a);
+        const sortedArray = sortArrayDescending(array_1);
+        console.log("Отсортированный массив в порядке убывания: ", sortedArray);
 
-const getFirstFiveSubArray = (array) => array.slice(0, 5);
+        const firstFiveSubArray = getFirstFiveSubArray(array_1);
+        console.log("Первые 5 элементов массива: ", firstFiveSubArray);
 
-const getLastFiveSubArray = (array) => array.slice(-5);
+        const lastFiveSubArray = getLastFiveSubArray(array_1);
+        console.log("Последние 5 элементов массива: ", lastFiveSubArray);
 
-const getSumEvenNumbers = (array) => array.filter(num => num % 2 === 0).reduce((sum, num) => sum + num, 0);
+        const evenNumbersSum = getSumEvenNumbers(array_1);
+        console.log("Сумма чётных чисел массива", evenNumbersSum);
 
-const getSquaresOfEvenNumbers = (array) => array.filter(num => num % 2 === 0).map(num => num * num);
+        const array_2 = createArray(100);
+        const squaresArray = getSquaresOfEvenNumbers(array_2);
+        console.log("Квадраты чётных чисел массива: ", squaresArray)
+    }
 
-main();
+    return {
+        main
+    };
+})();
+
+appMameSpace.main();
