9b09ff9256a25e783e881a6b6a0917fcdbd65804
piinko <piinko@deeplay.io>
2024-11-30 13:09:30 +0700

    Добавлена задача Работа с массивами

---
+++ JSArray/Array.js
@@ -0,0 +1,34 @@
+function main() {
+    const array_1 = createArray(10);
+    console.log("Исходный массив", array_1);
+
+    const sortedArray = sortArrayDescending(array_1);
+    console.log("Отсортированный массив в порядке убывания: ", sortedArray);
+
+    const firstFiveSubArray = getFirstFiveSubArray(array_1);
+    console.log("Первые 5 элементов массива: ", firstFiveSubArray);
+
+    const lastFiveSubArray = getLastFiveSubArray(array_1);
+    console.log("Последние 5 элементов массива: ", lastFiveSubArray);
+
+    const evenNumbersSum = getSumEvenNumbers(array_1);
+    console.log("Сумма чётных чисел массива", evenNumbersSum);
+
+    const array_2 = createArray(100);
+    const squaresArray = getSquaresOfEvenNumbers(array_2);
+    console.log("Квадраты чётных чисел массива: ", squaresArray)
+}
+
+const createArray = (length) => Array.from({length}, (_, i) => i + 1);
+
+const sortArrayDescending = (array) => array.sort((a, b) => b - a);
+
+const getFirstFiveSubArray = (array) => array.slice(0, 5);
+
+const getLastFiveSubArray = (array) => array.slice(-5);
+
+const getSumEvenNumbers = (array) => array.filter(num => num % 2 === 0).reduce((sum, num) => sum + num, 0);
+
+const getSquaresOfEvenNumbers = (array) => array.filter(num => num % 2 === 0).map(num => num * num);
+
+main();
