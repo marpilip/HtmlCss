7807440ccc27562de5566db73b04ebda2afb683e
piinko <piinko@deeplay.io>
2024-12-08 00:31:30 +0700

    Внесены изменения по задаче Работа с объектами

--- JSObject/Object.js
+++ JSObject/Object.js
@@ -1,4 +1,4 @@
-const CountryNamespace = (() => {
+const CountryNamespace = function() {
     const countries = [
         {
             name: 'USA',
@@ -6,8 +6,8 @@ const CountryNamespace = (() => {
                 {name: 'New York', population: 8550405},
                 {name: 'Los Angeles', population: 3990456},
                 {name: 'Chicago', population: 2746388},
-                {name: 'Philadelphia', population: 1608161},
-            ],
+                {name: 'Philadelphia', population: 1608161}
+            ]
         },
         {
             name: 'Canada',
@@ -16,8 +16,8 @@ const CountryNamespace = (() => {
                 {name: 'Montreal', population: 1769996},
                 {name: 'Vancouver', population: 675218},
                 {name: 'Calgary', population: 1392609},
-                {name: 'Edmonton', population: 1016193},
-            ],
+                {name: 'Edmonton', population: 1016193}
+            ]
         },
         {
             name: 'Mexico',
@@ -25,41 +25,39 @@ const CountryNamespace = (() => {
                 {name: 'Mexico City', population: 9209944},
                 {name: 'Guadalajara', population: 5002466},
                 {name: 'Monterrey', population: 5159506},
-                {name: 'Puebla', population: 3213283},
-            ],
+                {name: 'Puebla', population: 3213283}
+            ]
         },
         {
             name: 'Russia',
             cities: [
                 {name: 'Moscow', population: 12500000},
-                {name: 'Saint-Petersburg', population: 5000000},
-            ],
+                {name: 'Saint-Petersburg', population: 5000000}
+            ]
         },
     ];
 
-    const findCountriesWithMaxCities = () => {
-        const maxCities = Math.max(...countries.map(country => country.cities.length));
-        return countries.filter(country => country.cities.length === maxCities);
+    const findCountriesWithMaxCitiesNumber = (countries) => {
+        return countries.filter(country => country.cities.length === Math.max(...countries
+            .map(country => country.cities.length)));
     };
 
-    const calculateTotalPopulationByCountry = () => {
-        return countries.reduce((acc, country) => {
-            acc[country.name] = country.cities.reduce((total, city) => total + city.population, 0);
-            return acc;
+    const calculatePopulationByCountries = (countries) => {
+        return countries.reduce((populationByCountry, country) => {
+            populationByCountry[country.name] =
+                country.cities.reduce((countryNumbers, city) => countryNumbers + city.population, 0);
+            return populationByCountry;
         }, {});
-    }
+    };
 
     const main = () => {
-        const countriesWithMaxCities = findCountriesWithMaxCities();
-        const totalPopulationInfo = calculateTotalPopulationByCountry();
-
-        console.log('Страны с максимальным количеством городов:', countriesWithMaxCities);
-        console.log('Суммарная численность населения по странам:', totalPopulationInfo);
+        console.log('Страны с максимальным количеством городов:', findCountriesWithMaxCitiesNumber(countries));
+        console.log('Суммарная численность населения по странам:', calculatePopulationByCountries(countries));
     };
 
     return {
         main,
     };
-})();
+}();
 
 CountryNamespace.main();
