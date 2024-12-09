813a542f8c282efa2a53d4e52b0257cb5b501842
piinko <piinko@deeplay.io>
2024-12-09 21:51:07 +0700

    Внесены изменения по задаче Работа с массивами

--- JSArray/Array.js
+++ JSArray/Array.js
@@ -1,35 +1,35 @@
-const appMameSpace = (() => {
-    const createArray = (length) => Array.from({length}, (_, i) => i + 1);
+const AppNamespace = (function () {
+    const createArray = length =>
+        Array.from({length}, (_, i) => i + 1);
 
-    const sortArrayDescending = (array) => array.sort((a, b) => b - a);
+    const sortArrayDescending = array =>
+        array.sort((a, b) => b - a);
 
-    const getFirstFiveSubArray = (array) => array.slice(0, 5);
+    const getFirstNElementsSubarray = (array, elementsNumber) =>
+        array.slice(0, elementsNumber);
 
-    const getLastFiveSubArray = (array) => array.slice(-5);
+    const getLastNElementsSubarray = (array, elementsNumber) =>
+        array.slice(-elementsNumber);
 
-    const getSumEvenNumbers = (array) => array.filter(num => num % 2 === 0).reduce((sum, num) => sum + num, 0);
+    const getEvenNumbersSum = array =>
+        array.filter(number => number % 2 === 0)
+            .reduce((sum, evenNumber) =>
+                sum + evenNumber, 0);
 
-    const getSquaresOfEvenNumbers = (array) => array.filter(num => num % 2 === 0).map(num => num * num);
+    const getEvenNumbersSquares = array =>
+        array.filter(number => number % 2 === 0)
+            .map(evenNumber =>
+                evenNumber * evenNumber);
 
     const main = () => {
-        const array_1 = createArray(10);
-        console.log("Исходный массив", array_1);
+        const array = createArray(10);
 
-        const sortedArray = sortArrayDescending(array_1);
-        console.log("Отсортированный массив в порядке убывания: ", sortedArray);
-
-        const firstFiveSubArray = getFirstFiveSubArray(array_1);
-        console.log("Первые 5 элементов массива: ", firstFiveSubArray);
-
-        const lastFiveSubArray = getLastFiveSubArray(array_1);
-        console.log("Последние 5 элементов массива: ", lastFiveSubArray);
-
-        const evenNumbersSum = getSumEvenNumbers(array_1);
-        console.log("Сумма чётных чисел массива", evenNumbersSum);
-
-        const array_2 = createArray(100);
-        const squaresArray = getSquaresOfEvenNumbers(array_2);
-        console.log("Квадраты чётных чисел массива: ", squaresArray)
+        console.log("Исходный массив", createArray(10));
+        console.log("Отсортированный массив в порядке убывания: ", sortArrayDescending(array));
+        console.log("Первые 5 элементов массива: ", getFirstNElementsSubarray(array, 5));
+        console.log("Последние 5 элементов массива: ", getLastNElementsSubarray(array), 5);
+        console.log("Сумма чётных чисел массива", getEvenNumbersSum(array));
+        console.log("Квадраты чётных чисел массива: ", getEvenNumbersSquares(createArray(100)));
     }
 
     return {
@@ -37,4 +37,4 @@ const appMameSpace = (() => {
     };
 })();
 
-appMameSpace.main();
+AppNamespace.main();
